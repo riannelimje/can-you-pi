01e84224e4df4c48ccc9acd61d610f3899817910
Rianne Lim <rianne.limje@gmail.com>
2026-01-22 14:27:20 +0800

    feat: accumulated digits show in 1 line in cli

--- cli.py
+++ cli.py
@@ -1,5 +1,6 @@
 from game_logic import Game
 
+# TODO: find a way to type the numbers in a single line without pressing ENTERRRR
 def play_cli(): 
     game = Game()
     
@@ -7,6 +8,8 @@ def play_cli():
     print("Try to recall as many digits of Pi as you can.")
     print("Type 'exit' to quit the game at any time.")
     print("Starting: 3.", end="\n") 
+
+    pi_so_far = "3."
 
     while not game.is_game_over and game.current_index < len(game.pi_decimals):
         user_input = input().strip()
@@ -18,7 +21,9 @@ def play_cli():
             continue
         is_correct, expected_digit = game.check_input(user_input)
         if is_correct:
-            continue # i just wanna continue in the same line - alright seems like i still need to press enter
+            pi_so_far += user_input
+            print((pi_so_far), end="")
+            continue 
         else: 
             print(f"Game Over! The correct digit was: {expected_digit}")
             print(f"You've recalled {game.current_index} digits of Pi correctly.") # no need to minus 1 since i start from 0 
