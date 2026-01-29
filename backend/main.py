541a2ad12c894e18d394092f937860cae1423773
Rianne Lim <rianne.limje@gmail.com>
2026-01-29 14:44:57 +0800

    feat: add cors

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .routes import router
 
-app = FastAPI(title ="Can You Pi?")
+app = FastAPI(title="Can You Pi?")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(router)
 
 @app.get("/")
